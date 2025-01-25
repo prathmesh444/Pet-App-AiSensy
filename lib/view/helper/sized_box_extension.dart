5efa9c3a713190ff55dc1a1af9fddeff60ba8d9b
Prathmesh soni <prathmeshsoni6@gmail.com>
2025-01-25 16:30:27 +0530

    Delete lib directory

--- lib/view/helper/sized_box_extension.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-extension PercentSizedBox on SizedBox{
-   static SizedBox all(double heightWidthPercent,{Widget? child}) => SizedBox(height: Get.height * heightWidthPercent, width: Get.width * heightWidthPercent, child: child,);
-   static SizedBox width(double widthPercent,{Widget? child}) => SizedBox(width: Get.width * widthPercent, child: child,);
-   static SizedBox height(double heightPercent,{Widget? child}) => SizedBox(height: Get.height * heightPercent, child: child,);
-}
